1b553d25aff6571b6c10bef65986850d48e94cab
alvarosIGZ <alvaro.sanchez@intelygenz.com>
2017-09-28 16:43:48 +0200

    Add security group rules inside definition

--- main.tf
+++ main.tf
@@ -31,26 +31,20 @@ resource "aws_security_group" "instance"{
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
     }
-}
 
-resource "aws_security_group_rule" "http"{
-    security_group_id = "${aws_security_group.instance.id}"
-    type = "ingress"
-
+    ingress{
     from_port = 80
     to_port = 80
     protocol = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group_rule" "all_outbound"{
-    security_group_id = "${aws_security_group.instance.id}"
-    type = "egress"
-
+    }
+    
+    egress{
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "aws_instance" "example-instance" {
